fae604936400da79952c0886a6be5b461119409c
Szeraax <6242511+Szeraax@users.noreply.github.com>
2023-05-07 13:07:34 -0700

    init

--- src/Doorway.cs
+++ src/Doorway.cs
@@ -1,30 +1,300 @@
 using System.Net;
+using System.Text;
+using System.Web;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Discord.Rest;
+
+using Discord.Doorway.Lib;
+using static Discord.Doorway.Lib.helpers;
 
 namespace Discord.Doorway
 {
-    public class Doorway
+    public class Function
     {
         private readonly ILogger _logger;
 
-        public Doorway(ILoggerFactory loggerFactory)
+        public Function(ILoggerFactory loggerFactory)
         {
-            _logger = loggerFactory.CreateLogger<Doorway>();
+            _logger = loggerFactory.CreateLogger<Function>();
         }
 
         [Function("Doorway")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public MultiOutputBinding Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("C# HTTP trigger function received a request.");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            var httpresponse = req.CreateResponse(HttpStatusCode.OK);
+            httpresponse.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            var queryParams = HttpUtility.ParseQueryString(req.Url.Query);
+            StreamReader reader = new StreamReader(req.Body);
+            string text = reader.ReadToEnd();
+            _logger.LogInformation("Request: " + text);
+            response r = new response();
 
-            response.WriteString("Welcome to Azure Functions!");
+            // #region parse command body
+            discordInteraction d = new discordInteraction();
+            try
+            {
+                discordInteraction? parsed = JsonSerializer.Deserialize<discordInteraction>(text);
+                if (null == parsed)
+                {
+                    _logger.LogInformation("No input data");
+                    return new MultiOutputBinding()
+                    {
+                        HttpReponse = req.CreateResponse(HttpStatusCode.BadRequest)
+                    };
+                }
+                d.application_id = parsed.application_id;
+                d.type = parsed.type;
+                d.data = parsed.data;
+            }
+            catch
+            {
+                _logger.LogInformation("Couldn't parse via json deserializer");
 
-            return response;
+                try
+                {
+                    System.Collections.Specialized.NameValueCollection? parsed = HttpUtility.ParseQueryString(text);
+                    _logger.LogInformation("Keys detected: " + String.Join(" ", parsed.AllKeys));
+                    d.application_id = parsed["application_id"];
+                }
+                catch
+                {
+                    _logger.LogInformation("Couldn't parse via x-www-urlencoding deserializer either");
+                    httpresponse.StatusCode = HttpStatusCode.BadRequest;
+                    // response.WriteString("Bad input, application_id must be present and a string");
+                    return new MultiOutputBinding()
+                    {
+                        HttpReponse = httpresponse
+                    };
+                }
+            }
+            // #endregion
+
+
+
+
+            // #region Signature verification
+            if (req.Url.Host == "localhost" && req.Url.Port == 7071)
+            {
+                _logger.LogWarning("Skipping signature verification");
+            }
+            else
+            {
+                // Must reply with 401 unauthorized if no ed25519 signature or timestamp, or if bad validation
+                // https://discord.com/developers/docs/interactions/receiving-and-responding#security-and-authorization
+
+                string pubKey = GetEnvironmentVariable("APP_PUBLICKEY_" + d.application_id);
+                if (string.IsNullOrWhiteSpace(pubKey))
+                {
+                    _logger.LogInformation("No pubkey");
+                    httpresponse.StatusCode = HttpStatusCode.BadRequest;
+                    return new MultiOutputBinding()
+                    {
+                        HttpReponse = httpresponse
+                    };
+                }
+
+                var headers = req.Headers;
+                if (!headers.TryGetValues("X-Signature-Timestamp", out var timestamp) ||
+                    !headers.TryGetValues("X-Signature-Ed25519", out var signature))
+                {
+                    _logger.LogInformation("No timestamp or no sig");
+                    return new MultiOutputBinding()
+                    {
+                        HttpReponse = req.CreateResponse(HttpStatusCode.Unauthorized)
+                    };
+                }
+
+                _logger.LogInformation("Sig: " + signature.First());
+                _logger.LogInformation("ts: " + timestamp.First());
+
+                DiscordRestClient DiscordRestClient = new DiscordRestClient();
+                bool validated = false;
+                validated = DiscordRestClient.IsValidHttpInteraction(pubKey, signature.First(), timestamp.First(), text);
+
+                if (validated == false)
+                {
+                    _logger.LogInformation("Not valid signature");
+                    return new MultiOutputBinding()
+                    {
+                        HttpReponse = req.CreateResponse(HttpStatusCode.Unauthorized)
+                    };
+                }
+            }
+            // #endregion
+
+            // Respond to pings with ACK
+            if (d.type == 1)
+            {
+                _logger.LogInformation("Type 1 found!");
+                r.type = 1;
+                httpresponse.WriteString(JsonSerializer.Serialize(r));
+                return new MultiOutputBinding()
+                {
+                    HttpReponse = httpresponse
+                };
+            }
+
+            // #region Retrieve command name
+            // Build a string from the application command and then go and get that ENV variable.
+            // Parse it to a responseEmbed obj. Return!
+            // Example equivilent: GetLink("LINK_GET_BATTALIONS");
+            StringBuilder sb = new StringBuilder("APP_COMMAND_");
+            if (!string.IsNullOrWhiteSpace(d.data.name))
+            {
+                sb.Append(d.data.name);
+                if (
+                    null != d.data.options &&
+                    d.data.options.Any() &&
+                    !string.IsNullOrWhiteSpace(d.data.options[0].name) &&
+                    d.data.options[0].type <= 2
+                )
+                {
+                    sb.Append("_" + d.data.options[0].name);
+                    if (
+                        null != d.data.options[0].options &&
+                        d.data.options[0].options.Any() &&
+                        !string.IsNullOrWhiteSpace(d.data.options[0].options[0].name) &&
+                        d.data.options[0].options[0].type <= 2
+                    )
+                    {
+                        sb.Append("_" + d.data.options[0].options[0].name);
+                    }
+                }
+            }
+            string command = sb.ToString().ToUpper();
+            _logger.LogInformation("Command is: " + command);
+            // #endregion
+
+            // #region command canned response
+            if (string.IsNullOrWhiteSpace(command))
+            {
+                r.type = 5;
+                httpresponse.WriteString(JsonSerializer.Serialize(r));
+                return new MultiOutputBinding()
+                {
+                    HttpReponse = httpresponse,
+                    QueueBody = text
+                };
+            }
+            _logger.LogInformation("checking for canned for command");
+            string cannedResponse = "";
+            cannedResponse = GetEnvironmentVariable(command);
+            // Maybe enable application specific commands in the future:
+            // Maybe enable guild specific commands in the future too!
+            // if (string.IsNullOrWhiteSpace(cannedResponse))
+            // {
+            //     _logger.LogInformation("Check if there is an app specific canned response");
+            //     cannedResponse = GetEnvironmentVariable(command + "_" + d.application_id);
+            // }
+
+            if (string.IsNullOrWhiteSpace(cannedResponse))
+            {
+                r.type = 5;
+                httpresponse.WriteString(JsonSerializer.Serialize(r));
+                return new MultiOutputBinding()
+                {
+                    HttpReponse = httpresponse,
+                    QueueBody = text
+                };
+            }
+
+            _logger.LogInformation("Found a canned response! " + cannedResponse);
+            r.data = new responseData();
+            r.type = 4;
+            // standard canned command properties should get applied to the response as needed
+            try
+            {
+                CannedCommand? cc = JsonSerializer.Deserialize<CannedCommand>(cannedResponse);
+                if (null == cc)
+                {
+                    throw new NullReferenceException();
+                }
+                if (cc.@private == true)
+                {
+                    _logger.LogInformation("Adding private flag");
+                    r.data.flags = Lib.MessageFlags.EPHEMERAL;
+                }
+                if (cc.passthru == true)
+                {
+                    _logger.LogInformation("Doing passthru");
+                    r.type = 5;
+                }
+            }
+            catch
+            {
+                _logger.LogInformation("no private or passthru, skipping");
+            }
+
+
+            // free form content of canned command needs handled nicely
+            try
+            {
+                CannedCommandNested? ccn = JsonSerializer.Deserialize<CannedCommandNested>(cannedResponse);
+                if (null == ccn || null == ccn.content)
+                {
+                    throw new NullReferenceException();
+                }
+                cannedResponse = JsonSerializer.Serialize(ccn.content);
+
+            }
+            catch
+            {
+                try
+                {
+                    CannedCommandSimple? ccs = JsonSerializer.Deserialize<CannedCommandSimple>(cannedResponse);
+                    if (null == ccs || String.IsNullOrWhiteSpace(ccs.content))
+                    {
+                        throw new NullReferenceException();
+                    }
+                    cannedResponse = JsonSerializer.Serialize(ccs.content);
+                }
+                catch { }
+            }
+
+
+            // Need to handle various canned command possibilites
+            try
+            {
+                responseEmbed? da = JsonSerializer.Deserialize<responseEmbed>(cannedResponse);
+                if (null != da)
+                {
+                    List<responseEmbed> l = new List<responseEmbed>();
+                    l.Add(da);
+                    r.data.embeds = l.ToArray();
+                    _logger.LogInformation("Setting to embed mode for response");
+                }
+                else
+                {
+                    _logger.LogWarning("Command embed was literally null");
+                }
+            }
+            catch
+            {
+                _logger.LogInformation("Setting to content mode for response");
+                r.data.content = cannedResponse;
+            }
+            httpresponse.WriteString(JsonSerializer.Serialize(r));
+
+            if (r.type == 4)
+            {
+                return new MultiOutputBinding()
+                {
+                    HttpReponse = httpresponse
+                };
+            }
+            else
+            {
+                return new MultiOutputBinding()
+                {
+                    QueueBody = text,
+                    HttpReponse = httpresponse
+                };
+            }
         }
     }
 }
---
+++ src/Lib.cs
@@ -0,0 +1,121 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace Discord.Doorway.Lib
+{
+    public class helpers
+    {
+        public static string CannedResponse(string variable)
+        {
+            string value = GetEnvironmentVariable(variable);
+            if (null == value)
+            {
+                throw new KeyNotFoundException("Can't find that URI: " + variable);
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public static string GetEnvironmentVariable(string name)
+        {
+            string? @out = System.Environment.GetEnvironmentVariable(name);
+            if (null == @out)
+            {
+                return "";
+            }
+            else
+            {
+                return @out;
+            }
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+    }
+
+    public class MultiOutputBinding
+    {
+        [QueueOutput("{application_id}", Connection = "AzureWebJobsStorage")]
+        public string? QueueBody { get; set; }
+        public HttpResponseData HttpReponse { get; set; }
+    }
+    public class discordInteraction
+    {
+        public string application_id { get; set; }
+        public int type { get; set; }
+        public string guild_id { get; set; }
+        public discordInteractionData data { get; set; } // Shouldn't need
+    }
+
+    public class discordInteractionData
+    {
+        public string name { get; set; }
+        public int type { get; set; }
+        public string target_id { get; set; }
+        public discordInteractionDataOptions[] options { get; set; }
+    }
+
+    public class discordInteractionDataOptions
+    {
+        public string name { get; set; }
+        public int type { get; set; }
+        public string value { get; set; }
+        public discordInteractionDataOptions[] options { get; set; }
+    }
+
+    public class response
+    {
+        public int type { get; set; }
+        public responseData data { get; set; }
+    }
+    public class responseData
+    {
+        public string content { get; set; }
+        public MessageFlags flags { get; set; }
+        public responseEmbed[] embeds { get; set; }
+    }
+
+    public class responseEmbed
+    {
+        public string? title { get; set; }
+        public string? url { get; set; }
+        public string? description { get; set; }
+        public int? color { get; set; }
+    }
+
+    public class CannedCommand
+    {
+        public bool? @private { get; set; }
+        public bool? passthru { get; set; }
+    }
+    public class CannedCommandNested
+    {
+        public responseEmbed? content { get; set; }
+    }
+    public class CannedCommandSimple
+    {
+        public string? content { get; set; }
+    }
+
+    [System.Flags]
+    public enum MessageFlags
+    {
+        NONE = 0 << 0,
+        CROSSPOSTED = 1 << 0,
+        IS_CROSSPOST = 1 << 1,
+        SUPPRESS_EMBEDS = 1 << 2,
+        SOURCE_MESSAGE_DELETED = 1 << 3,
+        URGENT = 1 << 4,
+        HAS_THREAD = 1 << 5,
+        EPHEMERAL = 1 << 6,
+        LOADING = 1 << 7,
+        FAILED_TO_MENTION_SOME_ROLES_IN_THREAD = 1 << 8
+    }
+}
